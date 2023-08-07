55347dc1370abda32057691678c3140c71f1baea
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-08-07 23:00:33 +0300

    Edited allowed_hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -9,7 +9,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ecommerce-dj.onrender.com',
+    'localhost'
+]
 
 
 # Application definition
